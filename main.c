472861603224f15754ed4f97d20ae25c7c56850b
NechiforBogdan <71372147+NechiforBogdan@users.noreply.github.com>
2022-05-17 07:56:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 
 
 char buffer[4];
-int i = 0;
-int16 diz, unit;
+int i = 0; 
+int16 dizt, unite;
 boolean flag = 0;
-int16 maxDist = 100;
+int16 distMax = 100;
 int16 dist, time  ;
 int a, b,c;
 
@@ -18,14 +18,14 @@ void  TIMER1_isr(void)
 
 }
 #int_RDA
-void RDA_isr(void)
+void RDA_isr(void)   // gerer l'interuption
 {
-   buffer[i] = getc();
-    if(buffer[0] == ':' && flag == 0 ){
+   buffer[i] = getc();  // on lit la donnee qui arrive sur le port
+    if(buffer[0] == ':' && flag == 0 ){   // si le flag = 0 y a rien a faire, si flag = 1 y a une interruption et une donnee a traiter
         i++;
         if(i>=4){
             i = 0;
-            flag = 1;
+            flag = 1;      // 
         }
     }
  
@@ -39,7 +39,7 @@ void main()
    setup_adc(ADC_OFF);
    setup_psp(PSP_DISABLED);
    setup_spi(FALSE);
-   setup_wdt(WDT_OFF);
+   setup_wdt(WDT_OFF);     // watch dog timer off 
    setup_timer_0(RTCC_INTERNAL);
    setup_timer_1(T1_INTERNAL|T1_DIV_BY_8);
    setup_timer_2(T2_DISABLED,0,1);
@@ -65,11 +65,11 @@ void main()
       while(input(pin_c0)){} //on attend l'etat haut du pin echo
       time = get_timer1();               
       dist = time*0.028;  
-      printf("distance : %ld \n" , dist);
+      printf("la distance est : %ld \n" , dist);
       printf("\n");
       delay_ms(100);
     
-      if(dist<maxDist){
+      if(dist<distMax){ 
          output_high(PIN_E0);
          output_low(PIN_E1);
       }else{
@@ -79,26 +79,18 @@ void main()
       
       if(dist<100){
          output_high(pin_e2);
-         unit = dist%10;
-         diz =((dist - unit)/10)%100;
-         output_b((diz<<4)+unit);
+         unite = dist%10;
+         dizt =((dist - unite)/10)%100;
+         output_b((dizt<<4)+unite);
       }else{
          output_low(PIN_E2);
-         diz = dist/100;
-         unit = (dist - (diz*100))/10;
-         output_b((diz<<4)+unit);   // on decale de 4 bits
+         dizt = dist/100;
+         unite = (dist - (dizt*100))/10;
+         output_b((dizt<<4)+unite);   // on decale de 4 bits
          
         
       }
-      
-      if(flag == 1){
-         flag =0;
-         a = buffer[1]-48;
-         b = buffer[2]-48;
-         c = buffer[3]-48;
-         maxDist = (int16)(a*100+b*10+c);
-         
-      }
+
       delay_ms(300);
      }
 
